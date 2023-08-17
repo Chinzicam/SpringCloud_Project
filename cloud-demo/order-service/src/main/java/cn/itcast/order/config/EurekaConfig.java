76ed4f58fc7aa67a5397fc23318f92f99f39c856
Chinzicam <a3189797316@163.com>
2023-08-17 23:09:14 +0800

    :sparkles: Ribbon负载均衡

--- cloud-demo/order-service/src/main/java/cn/itcast/order/config/EurekaConfig.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/config/EurekaConfig.java
@@ -1,5 +1,7 @@
 package cn.itcast.order.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +13,9 @@ public class EurekaConfig {
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public IRule randomRule(){
+        return new RandomRule();
     }
 }
